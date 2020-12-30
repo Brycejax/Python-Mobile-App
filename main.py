9f290f5387553ec8cf37442af41b6681105fce84
Brycejax <bryce.martin726@hotmail.com>
2020-12-29 21:54:24 -0600

    v1.0.4: added functionality for displaying the quotes to the user. as well as imported some libraries to assist us with what we need.

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 import json
 from datetime import datetime
+import glob
+from pathlib import Path
+import random
 
 Builder.load_file('design.kv')
 
@@ -17,12 +20,21 @@ class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
 
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+        if uname in users and users[uname]["password"] == pword:
+            self.manager.current = "login_screen_success"
+        else:
+            #accessing a label
+            self.ids.login_wrong.text = "Wrong Username or Password"
+
 class RootWidget(ScreenManager):
     pass
 
 class SignUpScreen(Screen):
     def add_user(self, uname, pword):
-        with open("users.json", "r") as file:
+        with open("users.json") as file:
             users = json.load(file)
             #add to the dictonary
         users[uname] = {'username' : uname, 'password' : pword,
@@ -39,6 +51,24 @@ class SignUpScreenSuccess(Screen):
 
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.glob("quotes/*txt")
+
+        available_feelings = [Path(filename).stem for filename in available_feelings]
+
+        if feel in available_feelings:
+            with open(f"quotes/{feel}.txt") as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try a different feeling"
 
 class MainApp(App):
     def build(self):
