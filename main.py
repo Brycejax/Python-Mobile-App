46f8f3ebc6bcb81d5624f435a46efc8b7e8e428c
Brycejax <bryce.martin726@hotmail.com>
2020-12-29 18:53:45 -0600

    v1.0.1 added a signup page to link with our button. and got user input on press for the password and usernamefor the new screen.

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
-    pass
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
 
 class RootWidget(ScreenManager):
     pass
+
+class SignUpScreen(Screen):
+    def add_user(self, uname, pword):
+        print(uname,pword)
 
 class MainApp(App):
     def build(self):
