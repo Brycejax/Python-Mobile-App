8bc43afc24b0403127cf3a8e1ceb1040fa623402
Brycejax <bryce.martin726@hotmail.com>
2020-12-29 18:30:02 -0600

    v1.0.0: started a main.py script for logic and a .kv file for the use of kivy so that we can create a mobile app. So far I have created a simple button and label design.

---
+++ main.py
@@ -0,0 +1,25 @@
+#-----------------------------------------------
+# Developer----- Bryce Martin
+# Description---- This program will use Kivy
+#                 to create a simple mobile app
+#                 for the experience.
+#-----------------------------------------------
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file('design.kv')
+
+class LoginScreen(Screen):
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ == "__main__":
+    MainApp().run()
